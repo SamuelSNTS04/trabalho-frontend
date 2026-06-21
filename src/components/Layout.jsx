a0ef9b6680db8595b4a65004df53517b6dc71024
Daniel Pacheco Amorim <danielpachecoamorim@gmail.com>
2026-06-21 18:40:24 -0300

    feat: cria Navbar e integra no Layout como componente persistente #17

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+
 function Layout() {
-  return <></>;
+  return (
+    <div className="layout-container">
+      {/* A Navbar fica renderizada de forma fixa aqui */}
+      <Navbar />
+
+      {/* O <Outlet /> é o "buraco" onde as páginas vão ser injetadas sem recarregar o navegador */}
+      <main className="conteudo-principal">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default Layout;
