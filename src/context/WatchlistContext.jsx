7a3821ff6ba00bad886a51a671cb562277a8bbd1
SamuelSNTS04 <malakaigamer5@gmail.com>
2026-06-21 20:11:55 -0300

    feat: criando o arquivo WatchlistContext

---
+++ src/context/WatchlistContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+
+// 2.1 - Inicializar o createContext
+export const WatchlistContext = createContext();
+
+export function WatchlistProvider({ children }) {
+  // 2.3 - Criar o estado movies
+  const [movies, setMovies] = useState([]);
+
+  // 2.3 - Implementar o useEffect com fetch(GET) para carregar os dados do db.json
+  useEffect(() => {
+    fetch("http://localhost:3000/filmes")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro ao buscar filmes da API");
+        }
+        return response.json();
+      })
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Erro no fetch inicial:", error));
+  }, []);
+
+  // 2.4 - Criar a função addMovieToList usando o operador spread
+  const addMovieToList = (newMovie) => {
+    setMovies((prevMovies) => [...prevMovies, newMovie]);
+  };
+
+  // 2.6 - Fornecer o total de filmes (movies.length)
+  const totalMovies = movies.length;
+
+  return (
+    <WatchlistContext.Provider
+      value={{
+        movies,
+        setMovies,
+        addMovieToList,
+        totalMovies,
+      }}
+    >
+      {children}
+    </WatchlistContext.Provider>
+  );
+}
